cb172cc540bc3843080cc390732a218e16c62853
Robson Duarte <robson.o.d@gmail.com>
2016-04-06 21:26:07 -0300

    add simple modification of default wizard PDE .

--- br.com.mystudies.eclipse.plugin.hello.ui/src/br/com/mystudies/eclipse/plugin/hello/ui/handlers/SampleHandler.java
+++ br.com.mystudies.eclipse.plugin.hello.ui/src/br/com/mystudies/eclipse/plugin/hello/ui/handlers/SampleHandler.java
@@ -3,9 +3,9 @@ package br.com.mystudies.eclipse.plugin.hello.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -27,8 +27,8 @@ public class SampleHandler extends AbstractHandler {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(
 				window.getShell(),
-				"Hello",
-				"Hello, Eclipse world");
+				"Hello ui",
+				"Hello the first eclipse plguin ui");
 		return null;
 	}
 }
